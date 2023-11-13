65321c76c2f02d31c8dd10a086086efb2e4d0f36
ChinhMC <maichanchinhls@gmail.com>
2023-11-13 11:22:38 +0700

    update from create apps. Adhoc -> android

--- app/serializers/api/app_serializer.rb
+++ app/serializers/api/app_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::AppSerializer < ApplicationSerializer
-  attributes :id, :name
+  attributes :id, :name, :recently_release,:total_schemes, :total_channels, :total_releases
 
   has_many :schemes
 end
